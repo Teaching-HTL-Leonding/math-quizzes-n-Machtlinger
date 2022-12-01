7fe4e44ab2f000261352438620e70667730bf679
n-Machtlinger <113846174+n-Machtlinger@users.noreply.github.com>
2022-12-01 11:28:11 +0100

    Add files via upload

---
+++ 023_finding_Operator/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+
+Console.WriteLine("Please enter your first number: ");
+double num1 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Please enter your second number: ");
+double num2 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Please enter your result :");
+double result = double.Parse(Console.ReadLine()!);
+
+if (Math.Round(num1 + num2, 2) == result || Math.Round(num2 + num1, 2) == result)
+
+{
+    Console.WriteLine("The Operator is +");
+}
+else if (Math.Round(num1 - num2, 2) == result || Math.Round(num2 - num1, 2) == result)
+{
+    Console.WriteLine("The Operator is -");
+}
+else if (Math.Round(num1 * num2, 2) == result || Math.Round(num2 * num1, 2) == result)
+
+{
+    Console.WriteLine("The Operator is *");
+}
+else if (Math.Round(num1 / num2, 2) == result || Math.Round(num2 / num1, 2) == result)
+{
+    Console.WriteLine("The Operator is /");
+}
+else if (Math.Round(num1 % num2, 2) == result || Math.Round(num2 % num1, 2) == result)
+{
+    Console.WriteLine("The Operator is %");
+}
+else if (Math.Pow(num1 , num2 ) == result || Math.Pow(num2 , num1 ) == result)
+{
+    Console.WriteLine("The Operator is ^ ");
+}
